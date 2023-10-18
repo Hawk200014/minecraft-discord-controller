f4d26fb2ad2c351cf6742f5df9e6d31ec6a4d287
Hawk200014 <72735051+Hawk200014@users.noreply.github.com>
2023-10-18 10:59:36 +0200

    -added Singleton class -added spark as rest api -added RequestProcessor to process the requests

--- src/main/java/de/hawk200014/Main.java
+++ src/main/java/de/hawk200014/Main.java
@@ -2,11 +2,20 @@ package de.hawk200014;
 
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import spark.Request;
+import spark.Response;
+import spark.Route;
 
 public class Main {
     public static void main(String[] args)
     {
-        JDABuilder builder = JDABuilder.createDefault(args[0]);
+        initSingletons();
+        initDiscord(args[0]);
+        initApi();
+    }
+
+    public static void initDiscord(String key){
+        JDABuilder builder = JDABuilder.createDefault(key);
 
         // Enable the bulk delete event
         builder.setBulkDeleteSplittingEnabled(false);
@@ -15,4 +24,15 @@ public class Main {
 
         builder.build();
     }
+
+    public static void initApi(){
+        spark.Spark.post("/api", (request, response) -> {
+            return ((RequestProcessor)Singletons.getInstance().getSingleton("requestprocessor")).processData(request, response);
+        });
+    }
+
+    public static void initSingletons(){
+        new Singletons().addSingleton(new RequestProcessor(), "requestprocessor");
+    }
+
 }
